7d2885bfffe6c004b8446e4392aac1a6a1e94bc2
33shuhei-dev <imoimo33shuhei@gmail.com>
2026-03-30 21:05:32 +0900

    update

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from '../kansai-fishing-v2.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
